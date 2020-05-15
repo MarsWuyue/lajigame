9d5579cad37b5dac4d672dff8f1d572ac76dd10a
Wuyue <314493146@qq.com>
2020-05-15 13:10:09 +0800

    添加记分和手动重玩

--- main.d5c14.js
+++ main.d5c14.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.d040a.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.b44b2.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
@@ -179,7 +179,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.22140.js');
+        require('src/settings.25f09.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -187,7 +187,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.22140.js');
+        require('src/settings.25f09.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
