408fedfe81b7d79263edc413f32ef2d044b4ccb8
Wuyue <314493146@qq.com>
2020-05-17 11:43:36 +0800

    add music

--- main.2a013.js
+++ main.2a013.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.cfaae.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.211d5.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
