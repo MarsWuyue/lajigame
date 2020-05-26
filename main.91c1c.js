00c88a6b259141b139cac3dcc9b1b03fd36edb9d
Wuyue <314493146@qq.com>
2020-05-26 11:38:26 +0800

    add setting byutton

--- main.91c1c.js
+++ main.91c1c.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.14fd5.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.192ed.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
